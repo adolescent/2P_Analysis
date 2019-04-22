985fc8d329e86d945ad33c628daeee4e596f8565
adolescent <39371189+adolescent@users.noreply.github.com>
2019-04-22 14:11:10 +0800

    Main function defined
    Now this function can be run in a single function **.main()
    
    Cheers

--- Stimulus_Analysis/Step1_Align_Par.py
+++ Stimulus_Analysis/Step1_Align_Par.py
@@ -17,9 +17,10 @@ import dill
 class Align_Tifs:#定义类，即align
     name =r'Align_All_Frames'#定义class的属性，如果没有__init__的内容就会以这里的作为类属性。
     
-    def __init__(self,data_folder,show_gain):#初始化变量
+    def __init__(self,data_folder,show_gain,core_Num):#初始化变量
         self.data_folder = data_folder#文件目录
         self.show_gain = show_gain#对齐增益，GA取32，RG取256
+        self.core_Num = core_Num#用于多核运算的核心数，在这里指定。
         
     def path_cycle(self):#这里的变量从self自己那里来,需要returnall_tif_name和tif_Num
         self.all_tif_name = pp.tif_name(self.data_folder)
@@ -51,9 +52,10 @@ class Align_Tifs:#定义类，即align
         temp_biased_graph = np.pad(temp_tif,((20+y_bias,20-y_bias),(20+x_bias,20-x_bias)),'constant')
         biased_graph = temp_biased_graph[20:532,20:532]
         cv2.imwrite((self.aligned_frame_folder+'\\'+self.all_tif_name[i].split('\\')[-1]),biased_graph)
-    #接下来两个函数用于避免pickle/dill的时候报错
-    def pool_set(self,i):#将进程池设为成员变量，这样就可以在dill的时候删除了
-        self.pool = mp.Pool(i)
+    #接下来三个函数用于避免pickle/dill的时候报错
+    
+    def pool_set(self):#将进程池设为成员变量，这样就可以在dill的时候删除了
+        self.pool = mp.Pool(self.core_Num)
     def __getstate__(self):
         self_dict = self.__dict__.copy()
         del self_dict['pool']
@@ -61,27 +63,31 @@ class Align_Tifs:#定义类，即align
 
     def __setstate__(self, state):
         self.__dict__.update(state)
+    
+    def main(self):
+        self.path_cycle()
+        graph_before_align = self.frame_average(self.all_tif_name)
+        self.show_graph('Graph_Before_Align',graph_before_align)
+        print('Start Align...')
+        partial_align = partial(self.align,base_frame = graph_before_align)
+        self.pool_set()
+        self.pool.map(partial_align,range(len(self.all_tif_name)))
+        print('Frame_Align_Done!')
+        self.aligned_frame_name = pp.tif_name(self.aligned_frame_folder)#定义类变量，方便提取
+        self.graph_after_align = self.frame_average(self.aligned_frame_name)
+        self.show_graph('Graph_After_Align',self.graph_after_align)
 #%%运行部分
 '''有时间可以把以下运行内容包入这个class的函数，然后写一个主函数文件'''
 if __name__ == '__main__':
     start_time = time.time()#任务开始时间
     show_gain = 32#图片增益
-    run = Align_Tifs(r'F:\codes\190412_L74\test_data',show_gain)#主程序，在这里进行运算
-    run.path_cycle()
-    graph_before_align = run.frame_average(run.all_tif_name)
-    run.show_graph('Graph_Before_Align',graph_before_align)
-    print('Start Align...')
-    partial_align = partial(run.align,base_frame = graph_before_align)
-    #run.align(graph_before_align)
-    run.pool_set(3)
-    run.pool.map(partial_align,range(len(run.all_tif_name)))
-    print('Frame_Align_Done!')
-    aligned_frame_name = pp.tif_name(run.aligned_frame_folder)
-    graph_after_align = run.frame_average(aligned_frame_name)
-    run.show_graph('Graph_After_Align',graph_after_align)
+    data_folder = r'D:\datatemp\190412_L74\test_data'
+    run = Align_Tifs(data_folder,show_gain,3)
+    run.main()
     finish_time = time.time()
     print('Task Time Cost:'+str(finish_time-start_time)+'s')
     save_folder = run.save_folder
+    aligned_frame_name = run.aligned_frame_name
     #%%接下来将变量保存下来,保存在代码所在目录
     variable_name = 'Step1_Variable.pkl'
     dill.dump_session(variable_name)

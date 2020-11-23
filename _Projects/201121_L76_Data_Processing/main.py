79ef7453f780b34f7c93f826cb1a6a1b2099e67b
adolescent <39371189+adolescent@users.noreply.github.com>
2020-11-24 00:19:00 +0800

    Run test

--- _Projects/201121_L76_Data_Processing/main.py
+++ _Projects/201121_L76_Data_Processing/main.py
@@ -9,7 +9,7 @@ Codes to process L76-201121 Data
 from My_Wheels.Translation_Align_Function import Translation_Alignment
 import My_Wheels.List_Operation_Kit as List_Tools
 #%% First location
-data_folder=  [r'G:\Test_Data\2P\201121_L76_LM']
+data_folder=  [r'E:\Test_Data\2P\201121_L76_LM']
 run_name = [
     '1-001',
     '1-002',
@@ -17,5 +17,7 @@ run_name = [
     '1-004',
     '1-008',
     ]
+import cv2
+base = cv2.imread(r'E:\Test_Data\2P\201121_L76_LM\Loc1~8_Base.tif',-1)
 loc1_folders = List_Tools.List_Annex(data_folder, run_name)
-Translation_Alignment(loc1_folders,base_mode = 1)
+Translation_Alignment([loc1_folders[0]],base_mode = 'global',input_base = base,align_range = 20)

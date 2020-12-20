b84ef2dc004b47842a79bcd184be3b4476e5a896
adolescent <39371189+adolescent@users.noreply.github.com>
2020-12-21 00:44:22 +0800

    video improved

--- My_Wheels/Video_Writer.py
+++ My_Wheels/Video_Writer.py
@@ -7,53 +7,63 @@ Created on Sat Sep 19 15:53:39 2020
 import My_Wheels.OS_Tools_Kit as OS_Tools
 import My_Wheels.Graph_Operation_Kit as Graph_Tools
 import cv2
-import scipy
+import numpy as np
+import My_Wheels.Filters as Filters
 
 def Video_From_File(
         data_folder,
         graph_size = (512,512),
         file_type = '.tif',
         fps = 15,
-        clip = 5,
-        gaussian = 1,
+        gain = 20,
+        LP_Gaussian = ([5,5],1.5),
         frame_annotate = True,
+        cut_boulder = [20,20,20,20],
         ):
-    """
-    The function to fold all graphs in folder into a video file. All paremeters of video can be adjusted.
-    Video can only be shown as 8 bit, if input graph is 16bit, we need to change it into 8.
-    
+    '''
+    Write all files in a folder as a video.
+
     Parameters
     ----------
-    data_folder : (str)
-        Folder of all image files.
-    graph_size : (turple)
-        Size of graphs. Need to be pre-given for convenience. The default is (512，512)
-    file_type : (str), optional
-        Extend name of image files. The default is '.tif'.
-    fps : (int), optional
-        Frame per second. The default is 15.
-    clip : (float), optional
-        Std of clip for graph to average. The default is 5.
-    gaussian : (int), optional
-        Gaussian Blurry std. The bigger this vale, the more blurry will be. The default is 1.
-    frame_annotate : (bool),optional
-        Whether we show frame number on video. The default is true.
- 
+    data_folder : TYPE
+        DESCRIPTION.
+    graph_size : TYPE, optional
+        DESCRIPTION. The default is (512,512).
+    file_type : TYPE, optional
+        DESCRIPTION. The default is '.tif'.
+    fps : TYPE, optional
+        DESCRIPTION. The default is 15.
+    gain : TYPE, optional
+        DESCRIPTION. The default is 20.
+    LP_Gaussian : TYPE, optional
+        DESCRIPTION. The default is ([5,5],1.5).
+    frame_annotate : TYPE, optional
+        DESCRIPTION. The default is True.
+    cut_boulder : TYPE, optional
+        DESCRIPTION. The default is [20,20,20,20].
+     : TYPE
+        DESCRIPTION.
 
     Returns
-    True.
+    -------
+    bool
+        DESCRIPTION.
 
-    """
+    '''
 
     all_tif_name = OS_Tools.Get_File_Name(path = data_folder,file_type = file_type)
     graph_num = len(all_tif_name)
-    video_writer = cv2.VideoWriter(data_folder+r'\\Video.mp4',cv2.VideoWriter_fourcc('H','E','V','C'),fps,graph_size,0)
+    video_writer = cv2.VideoWriter(data_folder+r'\\Video.mp4',cv2.VideoWriter_fourcc('X','V','I','D'),fps,graph_size,0)
     #video_writer = cv2.VideoWriter(data_folder+r'\\Video.avi',-1,fps,graph_size,0)
     for i in range(graph_num):
         raw_graph = cv2.imread(all_tif_name[i],-1).astype('f8')
-        # Do clip first.
-        u1_writable_graph = Graph_Tools.Clip_And_Normalize(raw_graph,clip_std = clip,bit = 'u1')
-        u1_writable_graph = scipy.ndimage.gaussian_filter(u1_writable_graph,sigma = gaussian)
+        # Cut graph boulder.
+        raw_graph = Graph_Tools.Graph_Cut(raw_graph, cut_boulder)
+        # Do gain then
+        gained_graph = np.clip(raw_graph.astype('f8')*gain/256,0,255).astype('u1')
+        # Then do filter, then 
+        if LP_Gaussian != False:
+            u1_writable_graph = Filters.Filter_2D(gained_graph,LP_Gaussian,False)
         if frame_annotate == True:
             cv2.putText(u1_writable_graph,'Stim ID = '+str(i),(300,30),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(255),1)
         video_writer.write(u1_writable_graph)

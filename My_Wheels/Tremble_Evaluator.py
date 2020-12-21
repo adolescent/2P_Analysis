837725c5de74280e2ec9d83af874a850db853f66
adolescent <39371189+adolescent@users.noreply.github.com>
2020-12-21 23:45:13 +0800

    At home

--- My_Wheels/Tremble_Evaluator.py
+++ My_Wheels/Tremble_Evaluator.py
@@ -9,6 +9,7 @@ This file used to Evaluate align quality. Through mass center
 from My_Wheels.Graph_Cutter import Graph_Cutter
 import My_Wheels.OS_Tools_Kit as OS_Tools
 import My_Wheels.Graph_Operation_Kit as Graph_Tools
+import cv2
 
 def Tremble_Evaluator(
         data_folder,
@@ -16,10 +17,41 @@ def Tremble_Evaluator(
         boulder_ignore = 20,
         cut_shape = (4,4)
         ):
-    
+    '''
+    Evaluate tremble for graphs in given folder.
+
+    Parameters
+    ----------
+    data_folder : str
+        graph folder.
+    ftype : str, optional
+        Dtype of graphs. The default is '.tif'.
+    boulder_ignore : int, optional
+        Out part will be ignored. The default is 20.
+    cut_shape : TYPE, optional
+        Shape of graph cut. The default is (4,4).
+
+    Returns
+    -------
+    center_move_dic : TYPE
+        DESCRIPTION.
+    center_loc_dic : TYPE
+        DESCRIPTION.
+
+    '''
+    # Initialization
+    save_folder = data_folder+r'\Tremble_Evaluation'
+    OS_Tools.mkdir(save_folder)
     all_tif_name = OS_Tools.Get_File_Name(data_folder,file_type = ftype)
     average_graph = Graph_Tools.Average_From_File(all_tif_name)
-    schametic_graph,graph_location_list,after_size,cutted_graph_dics = Graph_Cutter(average_graph,boulder_ignore,cut_shape)
+    schametic_graph,_,_,_ = Graph_Cutter(average_graph,boulder_ignore,cut_shape)
+    Graph_Tools.Show_Graph(schametic_graph, 'Schametic_Graph', save_folder)
+    # then calculate center movement and center location.
+    center_move_dic = {}
+    centere_loc_dic = {}
     for i in range(len(all_tif_name)):
-        Graph_Cutter()
-    return Tremble_Dic
+        current_graph = cv2.imread(all_tif_name[i],-1)
+        _,_,_,current_cutted_graphs = Graph_Cutter(current_graph,boulder_ignore,cut_shape)
+        
+        
+    return center_move_dic,center_loc_dic

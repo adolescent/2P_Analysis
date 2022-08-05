ce14608a307a7f2c114d2db43770fba666ad65c1
adolescent <39371189+adolescent@users.noreply.github.com>
2022-08-05 16:17:09 +0800

    0805Done

--- _Projects/220711_For_Annual_Report/Cutted_Series_Regression.py
+++ _Projects/220711_For_Annual_Report/Cutted_Series_Regression.py
@@ -16,7 +16,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr,spearmanr
 import statsmodels.api as sm
 
 
@@ -63,8 +63,106 @@ sns.kdeplot(data = tempwin, x = 'Dist',y = 'Value',fill=True,thresh=0, levels=10
 sns.lineplot(X_fit,Y_fit,color = 'y')
 
 #%% for each timewindow, get OD/Orien corr here.
-window_ssa = pd.DataFrame(columns = ['SST','Dist','OD','Orien','Distr','ODr','Orienr'])
+winnum = pcwin76_used.shape[1]
+window_ssa = pd.DataFrame(columns = ['SST','Dist','OD','Orien','Distr','ODr','Orienr'],index = range(winnum))
+
+# use only corrs who have tuning.
+pcwin76_used = pcwin76_used.loc[tuned_lists,:]
+dist = pc_tuned['Dist']
+OD_diff = abs(pc_tuned['OD_A']-pc_tuned['OD_B'])
+raw_orien_diff = abs(pc_tuned['Orien_A']-pc_tuned['Orien_B'])
+raw_orien_diff[raw_orien_diff>90]=(180-raw_orien_diff)
+Orien_diff = raw_orien_diff
+
 
 for i in range(pcwin76_used.shape[1]):
-    pass
+    win_temp = pcwin76_used.iloc[:,i]
+    # fit dist first
+    X = dist
+    Y = win_temp
+    X = sm.add_constant(X)
+    model = sm.OLS(Y,X)
+    result = model.fit()
+    ess_dist = result.ess
+    c_sst = result.ess+result.ssr
+    r_dist = ess_dist/c_sst
+    # then fit orien 
+    X = Orien_diff
+    Y = win_temp
+    X = sm.add_constant(X)
+    model = sm.OLS(Y,X)
+    result = model.fit()
+    ess_orien = result.ess
+    r_orien = ess_orien/c_sst
+    # then fit OD
+    X = OD_diff
+    Y = win_temp
+    X = sm.add_constant(X)
+    model = sm.OLS(Y,X)
+    result = model.fit()
+    ess_od = result.ess
+    r_od = ess_od/c_sst
+    window_ssa.loc[i,:] = [c_sst,ess_dist,ess_od,ess_orien,r_dist,r_od,r_orien]
+    
+# plot graphs.
+plt.plot(window_ssa['SST'])
+plt.plot(window_ssa['OD'])
+plt.plot(window_ssa['Orien'])
+plt.plot(window_ssa['Dist'])
+#%% do the same on L85 and L91 corr pairs.
 
+pcinfo85 = ot.Load_Variable(wp,'pc85_info.pkl')
+pcwin85 = ot.Load_Variable(wp,'pc85win.pkl')
+pc_tuned = pcinfo85[pcinfo85.OD_A != -999]
+pc_tuned = pc_tuned[pc_tuned.OD_B != -999]
+pc_tuned = pc_tuned[pc_tuned.Orien_A != -999]
+pc_tuned = pc_tuned[pc_tuned.Orien_B != -999]
+tuned_lists = pc_tuned.index
+pcwin85_used = pcwin85.loc[tuned_lists,:]
+pcinfo85_used = pcinfo85.loc[tuned_lists,:]
+# get ssa analysis.
+winnum = pcwin85_used.shape[1]
+window_ssa85 = pd.DataFrame(columns = ['SST','Dist','OD','Orien','Distr','ODr','Orienr'],index = range(winnum))
+
+# use only corrs who have tuning.
+dist = pcinfo85_used['Dist']
+OD_diff = abs(pcinfo85_used['OD_A']-pcinfo85_used['OD_B'])
+raw_orien_diff = abs(pcinfo85_used['Orien_A']-pcinfo85_used['Orien_B'])
+raw_orien_diff[raw_orien_diff>90]=(180-raw_orien_diff)
+Orien_diff = raw_orien_diff
+
+
+for i in range(pcwin85_used.shape[1]):
+    win_temp = pcwin85_used.iloc[:,i]
+    # fit dist first
+    X = dist
+    Y = win_temp
+    X = sm.add_constant(X)
+    model = sm.OLS(Y,X)
+    result = model.fit()
+    ess_dist = result.ess
+    c_sst = result.ess+result.ssr
+    r_dist = ess_dist/c_sst
+    # then fit orien 
+    X = Orien_diff
+    Y = win_temp
+    X = sm.add_constant(X)
+    model = sm.OLS(Y,X)
+    result = model.fit()
+    ess_orien = result.ess
+    r_orien = ess_orien/c_sst
+    # then fit OD
+    X = OD_diff
+    Y = win_temp
+    X = sm.add_constant(X)
+    model = sm.OLS(Y,X)
+    result = model.fit()
+    ess_od = result.ess
+    r_od = ess_od/c_sst
+    window_ssa85.loc[i,:] = [c_sst,ess_dist,ess_od,ess_orien,r_dist,r_od,r_orien]
+    
+# plot graphs.
+plt.plot(window_ssa85['SST'])
+plt.plot(window_ssa85['OD'][60:])
+plt.plot(window_ssa85['Orien'][60:])
+plt.plot(window_ssa85['Distr'])

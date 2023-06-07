2e960d95b7c63538f36666822c7a85f339682393
adolescent <39371189+adolescent@users.noreply.github.com>
2023-06-07 17:08:08 +0800

    Raw format cell done
    After this, we need to finish stim cell.

--- My_Wheels/Cell_Class/Format_Cell.py
+++ My_Wheels/Cell_Class/Format_Cell.py
@@ -11,6 +11,9 @@ import pandas as pd
 from Filters import Signal_Filter
 import matplotlib.pyplot as plt
 from Decorators import Timer
+import cv2
+import copy
+from My_Wheels.Cell_Tools.Cell_Visualization import Cell_Weight_Visualization
 
 #%%
 class Cell(object):
@@ -28,6 +31,7 @@ class Cell(object):
         self.filter_para = filter_para
         self.fps = fps
         self.clip_std = clip_std
+        self.global_avr = cv2.imread(ot.join(self.wp,'Global_Average_cai.tif'))
         # Get real id of each run. If False, print and ignore 
         if orien != False:
             self.orienrun = '1-'+str(1000+orien)[1:]
@@ -43,6 +47,7 @@ class Cell(object):
             self.color = False
         # Generate Z frames as an preprocessing. This is the first process of all works.
         _ = self.Get_Z_Frames()
+        _ = self.Get_Cell_Loc()
         
     def __getitem__(self,key):# return a dictionary of specific cell trains. 
         return self.all_cell_dic[key]
@@ -69,19 +74,40 @@ class Cell(object):
             self.Z_Frames[c_run] = c_frame
         return self.Z_Frames
     
-    def Get_Cell_Loc(self):
-        pass
+    def Get_Cell_Loc(self): # This extract cell location alone, save space.
+        if hasattr(self,'Cell_Locs'):
+            print('Cell_Locations have already been extracted. ')
+            return self.Cell_Locs
+        self.Cell_Locs = pd.DataFrame(index=['Y','X'],columns=self.acn)
+        for i,cc in enumerate(self.acn):
+            cc_loc = self.all_cell_dic[cc]['Cell_Loc'].astype('i4')
+            self.Cell_Locs[cc] = cc_loc
+        return self.Cell_Locs
     
-    def Show_Cell(self,cellname,on_avr = True): # This show all cells out. if on_avr == True, stack cell on average graph.
-        pass
+    def Annotate_Cell(self,namelist,on_avr = True,name = True): # This show all cells out. if on_avr == True, stack cell on average graph.
+        if on_avr == True:
+            annotate_graph = copy.deepcopy(self.global_avr)
+        else:
+            annotate_graph = np.zeros(shape= (512,512,3),dtype = 'u1')
+        for i,cc in enumerate(namelist):
+            cc_loc = self.Cell_Locs[cc]
+            annotate_graph = cv2.circle(annotate_graph,(cc_loc['X'],cc_loc['Y']),7,(255,0,0),2)
+            if name == True:
+                annotate_graph = cv2.putText(annotate_graph,str(cc),(cc_loc['X']+7,cc_loc['Y']+7),cv2.FONT_HERSHEY_DUPLEX,0.3,(255,0,0),1,cv2.LINE_AA)
+        return annotate_graph
     
-    def Generate_Weighted_Cell(self,weight,on_avr = False): # weight need to be a pd array with name. Used for graph generation.
-        pass
+    def Generate_Weighted_Cell(self,weight,on_avr = False): # Actually is a direct usage of cell weight visualization
+        visualized_graph = Cell_Weight_Visualization(weight,acd = self.all_cell_dic)
+        return visualized_graph
+    
     def Save_Class(self,path='Default',name='Cell_Class'): # save the class. Get processed datas.
-        pass
+        if path == 'Default':
+            ot.Save_Variable(self.wp,name,self)
+        else:
+            ot.Save_Variable(path,name,self)
     
     
 #%% Test parts
 if __name__ == '__main__':
     day_folder = r'E:\2P_Raws\220630_L76_2P'
-    cc = Cell(day_folder)
+    test_cell = Cell(day_folder)

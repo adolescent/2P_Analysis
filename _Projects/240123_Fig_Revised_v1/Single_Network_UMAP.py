b23d2d8c7ed75729c916e05edd69ba2117f40d32
adolescent <39371189+adolescent@users.noreply.github.com>
2024-01-26 18:13:43 +0800

    G16 Only Embedding

---
+++ _Projects/240123_Fig_Revised_v1/Single_Network_UMAP.py
@@ -0,0 +1,298 @@
+'''
+This part will use umap train on G16 and OD map seperately.
+
+'''
+
+#%%
+from Cell_Class.Stim_Calculators import Stim_Cells
+from Cell_Class.Format_Cell import Cell
+import OS_Tools_Kit as ot
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from tqdm import tqdm
+import cv2
+from Kill_Cache import kill_all_cache
+from sklearn.model_selection import cross_val_score
+from sklearn import svm
+import umap
+import umap.plot
+from scipy.stats import pearsonr
+import scipy.stats as stats
+from Cell_Class.Plot_Tools import Plot_3D_With_Labels
+import copy
+from Cell_Class.Advanced_Tools import *
+from Cell_Class.UMAP_Classifier_Analyzer import *
+from Cell_Class.Timecourse_Analyzer import *
+
+
+work_path = r'D:\_Path_For_Figs\240123_Graph_Revised_v1\Fig1_Revised'
+expt_folder = r'D:\_All_Spon_Data_V1\L76_18M_220902'
+ac = ot.Load_Variable_v2(expt_folder,'Cell_Class.pkl')
+ac.wp = expt_folder
+c_spon = ot.Load_Variable(expt_folder,'Spon_Before.pkl')
+c_model = ot.Load_Variable(expt_folder,'All_Stim_UMAP_3D_20comp.pkl')
+ac.Regenerate_Cell_Graph()
+all_path_dic = list(ot.Get_Sub_Folders(r'D:\_All_Spon_Data_V1'))
+all_path_dic.pop(4)
+all_path_dic.pop(6)
+
+import warnings
+warnings.filterwarnings("ignore")
+
+#%% ######################## 1. EXAMPLE LOCATION G16 EMBEDDING #########################
+#%% 1. Train G16 only model and get od,g16,spon embeddings
+g16_frames,g16_ids = ac.Combine_Frame_Labels(od = False,color = False)
+od_frames,od_ids = ac.Combine_Frame_Labels(od = True,orien = False,color = False)
+model_orien = umap.UMAP(n_components=3,n_neighbors=20)
+model_orien.fit(g16_frames)
+
+# g16_embeddings = model_orien.transform(g16_frames)
+# spon_embeddings = model_orien.transform(c_spon)
+# od_embeddings = model_orien.transform(od_frames)
+#%% 2. Use Analyzer get Recover Map and classify IDs.
+analyzer_orien = UMAP_Analyzer(ac = ac,umap_model = model_orien,spon_frame = c_spon,orien = True,od = False,color = False)
+analyzer_orien.Train_SVM_Classifier(C = 1)
+g16_embeddings = analyzer_orien.stim_embeddings
+g16_ids = analyzer_orien.stim_label
+spon_embeddings = analyzer_orien.spon_embeddings
+spon_ids = analyzer_orien.spon_label
+#%% 3. Plot Stim and Spon Compare G16 Graphs.
+import matplotlib.colors as mcolors
+import matplotlib as mpl
+import colorsys
+plt.clf()
+plt.cla()
+fig,ax = plt.subplots(nrows=1, ncols=2,figsize = (10,6),dpi = 180,subplot_kw=dict(projection='3d'))
+## limit and title.
+orien_elev = 30
+orien_azim = 100
+for i in range(2):
+    ax[i].set_xlabel('UMAP 1')
+    ax[i].set_ylabel('UMAP 2')
+    ax[i].set_zlabel('UMAP 3')
+    ax[i].grid(False)
+    ax[i].view_init(elev=orien_elev, azim=orien_azim)
+    ax[i].axes.set_xlim3d(left=0.5, right=9.5) 
+    ax[i].axes.set_ylim3d(bottom=-4, top=7) 
+    ax[i].axes.set_zlim3d(bottom=-3, top=4) 
+## get orien color bars.
+color_setb = np.zeros(shape = (8,3))
+for i,c_orien in enumerate(np.arange(0,180,22.5)):
+    c_hue = c_orien/180
+    c_lightness = 0.5
+    c_saturation = 1
+    color_setb[i,:] = colorsys.hls_to_rgb(c_hue, c_lightness, c_saturation)
+cax_b = fig.add_axes([0.99, 0.4, 0.02, 0.3])
+custom_cmap = mcolors.ListedColormap(color_setb)
+bounds = np.arange(0,202.5,22.5)
+norm = mpl.colors.BoundaryNorm(bounds, custom_cmap.N)
+c_bar = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=custom_cmap),cax=cax_b, label='Orientation')
+c_bar.set_ticks(np.arange(0,180,22.5)+11.25)
+c_bar.set_ticklabels(np.arange(0,180,22.5))
+c_bar.ax.tick_params(size=0)
+## colorize stims
+rest_stim,_ = Select_Frame(g16_embeddings,g16_ids,used_id=[0])
+orien_stim,orien_stim_id = Select_Frame(g16_embeddings,g16_ids,used_id=list(range(9,17)))
+orien_color_stim = np.zeros(shape = (len(orien_stim_id),3),dtype='f8')
+for i,c_id in enumerate(orien_stim_id):
+    orien_color_stim[i,:] = color_setb[int(c_id)-9,:]
+ax[0].scatter3D(rest_stim[:,0],rest_stim[:,1],rest_stim[:,2],s = 1,c = [0.7,0.7,0.7],alpha = 0.1)
+ax[0].scatter3D(orien_stim[:,0],orien_stim[:,1],orien_stim[:,2],s = 1,c = orien_color_stim)
+## colorize spons
+rest_spon,_ = Select_Frame(spon_embeddings,spon_ids,used_id=[0])
+orien_spon,orien_spon_id = Select_Frame(spon_embeddings,spon_ids,used_id=list(range(9,17)))
+orien_color_spon = np.zeros(shape = (len(orien_spon_id),3),dtype='f8')
+for i,c_id in enumerate(orien_spon_id):
+    orien_color_spon[i,:] = color_setb[int(c_id)-9,:]
+ax[1].scatter3D(rest_spon[:,0],rest_spon[:,1],rest_spon[:,2],s = 1,c = [0.7,0.7,0.7],alpha = 0.1)
+ax[1].scatter3D(orien_spon[:,0],orien_spon[:,1],orien_spon[:,2],s = 1,c = orien_color_spon)
+ax[0].set_title('Stimulus Embedding in UMAP Space',size = 14)
+ax[1].set_title('Spontaneous Embedding in UMAP Space',size = 14)
+fig.tight_layout()
+
+#%% 4. Recover of stim maps only.
+analyzer_orien.Get_Stim_Spon_Compare(od = False,color=False)
+compare_graphs = analyzer_orien.compare_recover
+value_max = 2
+value_min = -1
+font_size = 10
+plt.clf()
+plt.cla()
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10,5),dpi = 180)
+cbar_ax = fig.add_axes([.99, .15, .02, .7])
+sns.heatmap(compare_graphs['Orien0'],center = 0,xticklabels=False,yticklabels=False,ax = axes[0,0],vmax = value_max,vmin = value_min,cbar_ax= cbar_ax,square=True)
+sns.heatmap(compare_graphs['Orien45'],center = 0,xticklabels=False,yticklabels=False,ax = axes[0,1],vmax = value_max,vmin = value_min,cbar_ax= cbar_ax,square=True)
+sns.heatmap(compare_graphs['Orien90'],center = 0,xticklabels=False,yticklabels=False,ax = axes[1,0],vmax = value_max,vmin = value_min,cbar_ax= cbar_ax,square=True)
+sns.heatmap(compare_graphs['Orien135'],center = 0,xticklabels=False,yticklabels=False,ax = axes[1,1],vmax = value_max,vmin = value_min,cbar_ax= cbar_ax,square=True)
+
+axes[0,0].set_title('Orientation0 Stimulus     Orientation0 Recovered',size = font_size)
+axes[0,1].set_title('Orientation45 Stimulus     Orientation45 Recovered',size = font_size)
+axes[1,0].set_title('Orientation90 Stimulus     Orientation90 Recovered',size = font_size)
+axes[1,1].set_title('Orientation135 Stimulus     Orientation135 Recovered',size = font_size)
+
+plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.05, hspace=None)
+fig.tight_layout()
+plt.show()
+#%% 5. Similarity stats.
+orien_similar,orien_similar_rand = analyzer_orien.Similarity_Compare_All(id_lists=list(range(9,17)))
+distribution_frame = pd.DataFrame(columns = ['Pearson R','Response Pattern','Data'])
+for i,c_cond in enumerate(['Orientation']):
+    c_data = [orien_similar][i]
+    c_rand = [orien_similar_rand][i]
+    for j,c_frame in enumerate(c_data):
+        distribution_frame.loc[len(distribution_frame)] = [c_frame,c_cond,'Data']
+    for j,c_frame in enumerate(c_rand):
+        distribution_frame.loc[len(distribution_frame)] = [c_frame,c_cond,'Random']
+# violin plot here.
+plt.clf()
+plt.cla()
+fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(2.5,4),dpi = 180)
+axes.axhline(y = 0,color='gray', linestyle='--')
+sns.violinplot(data=distribution_frame, x="Response Pattern", y="Pearson R",order = ['Orientation'],hue = 'Data',split=True, inner="quart",ax = axes,dodge= True,width=0.5)
+axes.set_title('Spontaneous Repeat Similarity',size = 10)
+axes.legend_.remove()
+plt.tight_layout()
+plt.show()
+
+
+#%% 6. Add shuffled results.
+shuffled_spon = Spon_Shuffler(c_spon)
+analyzer_orien_s = UMAP_Analyzer(ac = ac,umap_model = model_orien,spon_frame = shuffled_spon,orien = True,od = False,color = False)
+analyzer_orien_s.Train_SVM_Classifier(C = 1)
+spon_embeddings_s = analyzer_orien_s.spon_embeddings
+spon_ids_s = analyzer_orien_s.spon_label
+
+#Plot Stim and Spon Compare G16 Graphs,with Shuffle included.
+import matplotlib.colors as mcolors
+import matplotlib as mpl
+import colorsys
+plt.clf()
+plt.cla()
+fig,ax = plt.subplots(nrows=1, ncols=3,figsize = (14,6),dpi = 180,subplot_kw=dict(projection='3d'))
+## limit and title.
+orien_elev = 20
+orien_azim = 100
+for i in range(3):
+    ax[i].set_xlabel('UMAP 1')
+    ax[i].set_ylabel('UMAP 2')
+    ax[i].set_zlabel('UMAP 3')
+    ax[i].grid(False)
+    ax[i].view_init(elev=orien_elev, azim=orien_azim)
+    ax[i].axes.set_xlim3d(left=0.5, right=9.5) 
+    ax[i].axes.set_ylim3d(bottom=-4, top=7) 
+    ax[i].axes.set_zlim3d(bottom=-3, top=4) 
+## get orien color bars.
+color_setb = np.zeros(shape = (8,3))
+for i,c_orien in enumerate(np.arange(0,180,22.5)):
+    c_hue = c_orien/180
+    c_lightness = 0.5
+    c_saturation = 1
+    color_setb[i,:] = colorsys.hls_to_rgb(c_hue, c_lightness, c_saturation)
+cax_b = fig.add_axes([0.99, 0.4, 0.02, 0.3])
+custom_cmap = mcolors.ListedColormap(color_setb)
+bounds = np.arange(0,202.5,22.5)
+norm = mpl.colors.BoundaryNorm(bounds, custom_cmap.N)
+c_bar = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=custom_cmap),cax=cax_b, label='Orientation')
+c_bar.set_ticks(np.arange(0,180,22.5)+11.25)
+c_bar.set_ticklabels(np.arange(0,180,22.5))
+c_bar.ax.tick_params(size=0)
+## colorize stims
+rest_stim,_ = Select_Frame(g16_embeddings,g16_ids,used_id=[0])
+orien_stim,orien_stim_id = Select_Frame(g16_embeddings,g16_ids,used_id=list(range(9,17)))
+orien_color_stim = np.zeros(shape = (len(orien_stim_id),3),dtype='f8')
+for i,c_id in enumerate(orien_stim_id):
+    orien_color_stim[i,:] = color_setb[int(c_id)-9,:]
+ax[0].scatter3D(rest_stim[:,0],rest_stim[:,1],rest_stim[:,2],s = 1,c = [0.7,0.7,0.7],alpha = 0.1)
+ax[0].scatter3D(orien_stim[:,0],orien_stim[:,1],orien_stim[:,2],s = 1,c = orien_color_stim)
+## colorize spons
+rest_spon,_ = Select_Frame(spon_embeddings,spon_ids,used_id=[0])
+orien_spon,orien_spon_id = Select_Frame(spon_embeddings,spon_ids,used_id=list(range(9,17)))
+orien_color_spon = np.zeros(shape = (len(orien_spon_id),3),dtype='f8')
+for i,c_id in enumerate(orien_spon_id):
+    orien_color_spon[i,:] = color_setb[int(c_id)-9,:]
+ax[1].scatter3D(rest_spon[:,0],rest_spon[:,1],rest_spon[:,2],s = 1,c = [0.7,0.7,0.7],alpha = 0.1)
+ax[1].scatter3D(orien_spon[:,0],orien_spon[:,1],orien_spon[:,2],s = 1,c = orien_color_spon)
+## colorize shuffles.
+rest_shuffle,_ = Select_Frame(spon_embeddings_s,spon_ids_s,used_id=[0])
+orien_shuffle,orien_shuffle_id = Select_Frame(spon_embeddings_s,spon_ids_s,used_id=list(range(9,17)))
+orien_color_shuffle = np.zeros(shape = (len(orien_shuffle_id),3),dtype='f8')
+for i,c_id in enumerate(orien_shuffle_id):
+    orien_color_shuffle[i,:] = color_setb[int(c_id)-9,:]
+ax[2].scatter3D(rest_shuffle[:,0],rest_shuffle[:,1],rest_shuffle[:,2],s = 1,c = [0.7,0.7,0.7],alpha = 0.1)
+ax[2].scatter3D(orien_shuffle[:,0],orien_shuffle[:,1],orien_shuffle[:,2],s = 1,c = orien_color_shuffle)
+# titles
+ax[0].set_title('Stimulus Embedding in UMAP Space',size = 14)
+ax[1].set_title('Spontaneous Embedding in UMAP Space',size = 14)
+ax[2].set_title('Shuffled Embedding in UMAP Space',size = 14)
+
+fig.tight_layout()
+
+
+#%%############################# ALL LOCATION STATS ##################################
+# 1. Generate all repeat with similarity.
+for i,c_loc in tqdm(enumerate(all_path_dic)):
+    c_loc_last = c_loc.split('\\')[-1]
+    c_spon_frame = ot.Load_Variable(c_loc,'Spon_Before.pkl')
+    c_ac = ot.Load_Variable_v2(c_loc,'Cell_Class.pkl')
+    # trian reducer with only G16 data.
+    g16_frames,g16_ids = c_ac.Combine_Frame_Labels(od = False,color = False)
+    model_orien = umap.UMAP(n_components=3,n_neighbors=20)
+    model_orien.fit(g16_frames)
+    c_analyzer = UMAP_Analyzer(ac = c_ac,umap_model=model_orien,spon_frame=c_spon_frame)
+    c_analyzer.Train_SVM_Classifier(C = 1)
+    c_analyzer.Similarity_Compare_Average(od = False,color = False)
+    c_recover_similar = c_analyzer.Avr_Similarity
+    c_recover_similar['Location'] = c_loc_last
+    if i == 0:
+        all_recover_similarity = copy.deepcopy(c_recover_similar)
+    else:
+        all_recover_similarity = pd.concat([all_recover_similarity,c_recover_similar],ignore_index=True)
+#%% 2.Plot averaged maps' similarity
+plt.clf()
+plt.cla()
+# set graph
+fig,ax = plt.subplots(nrows=1, ncols=1,figsize = (2.5,5),dpi = 180)
+ax.axhline(y = 0,color='gray', linestyle='--')
+sns.boxplot(data = all_recover_similarity,x = 'Data',y = 'PearsonR',hue = 'Network',ax = ax,showfliers = False)
+ax.set_title('Network Repeat Similarity')
+ax.set_xlabel('')
+ax.legend(title = 'Network',fontsize = 8)
+ax.set_xticklabels(['Real Data','Random Select'],size = 7)
+ax.set_ylabel('Pearson R')
+plt.show()
+
+#%% ########################## ALL REPEAT FREQUENCY STATS ###################################
+spon_repeat_count = pd.DataFrame(columns=['Loc','Network','Repeat_Freq','Repeat_Num','Data'])
+for i,c_loc in tqdm(enumerate(all_path_dic)):
+    c_loc_last = c_loc.split('\\')[-1]
+    c_spon_frame = ot.Load_Variable(c_loc,'Spon_Before.pkl')
+    c_ac = ot.Load_Variable_v2(c_loc,'Cell_Class.pkl')
+    g16_frames,g16_ids = c_ac.Combine_Frame_Labels(od = False,color = False)
+    model_orien = umap.UMAP(n_components=3,n_neighbors=20)
+    model_orien.fit(g16_frames)
+    c_analyzer = UMAP_Analyzer(ac = c_ac,umap_model=model_orien,spon_frame=c_spon_frame)
+    c_analyzer.Train_SVM_Classifier(C = 1)
+    c_analyzer.Similarity_Compare_Average()
+    c_spon_series = c_analyzer.spon_label
+    tc_analyzer = Series_TC_info(input_series=c_spon_series,od = False,color= False)
+    _,c_orien_freq,_ = tc_analyzer.Freq_Estimation(type='Event')
+    _,c_orien_num,_ = tc_analyzer.Freq_Estimation(type='Frame')
+    # and shuffle network repeat freq.
+    shuffle_times = 10
+    repeat_freq_s = np.zeros(shape = (shuffle_times,1),dtype = 'f8')# save in sequence od,orien,color.
+    for j in range(shuffle_times):# shuffle
+        spon_frame_s = Spon_Shuffler(c_spon_frame,method='phase')
+        spon_embedding_s = model_orien.transform(spon_frame_s)
+        c_spon_label_s = SVC_Fit(c_analyzer.svm_classifier,data = spon_embedding_s,thres_prob = 0)
+        tc_analyzer_s = Series_TC_info(input_series=c_spon_label_s,od = False,color = False)
+        _,c_orien_freq_s,_ = tc_analyzer_s.Freq_Estimation(type='Event')
+        _,c_orien_num_s,_ = tc_analyzer_s.Freq_Estimation(type='Frame')
+        repeat_freq_s[j,:] = [c_orien_freq_s]
+    repeat_freq_s = repeat_freq_s.mean(0)
+    spon_repeat_count.loc[len(spon_repeat_count),:] = [c_loc_last,'Orientation',c_orien_freq,c_orien_num,'Real']
+    spon_repeat_count.loc[len(spon_repeat_count),:] = [c_loc_last,'Orientation',c_orien_freq_s,c_orien_num_s,'Shuffle']
+
+#%% visualization
+    
--- _Projects/240123_Fig_Revised_v1/Strength_Decay.py
+++ _Projects/240123_Fig_Revised_v1/Strength_Decay.py
@@ -32,9 +32,120 @@ work_path = r'D:\_Path_For_Figs\240123_Graph_Revised_v1\Fig1_Revised'
 expt_folder = r'D:\_All_Spon_Data_V1\L76_18M_220902'
 ac = ot.Load_Variable_v2(expt_folder,'Cell_Class.pkl')
 ac.wp = expt_folder
-global_avr = cv2.imread(f'{expt_folder}\Global_Average_cai.tif',0) # read as 8 bit gray scale map.
 c_spon = ot.Load_Variable(expt_folder,'Spon_Before.pkl')
+c_model = ot.Load_Variable(expt_folder,'All_Stim_UMAP_3D_20comp.pkl')
 ac.Regenerate_Cell_Graph()
+all_path_dic = list(ot.Get_Sub_Folders(r'D:\_All_Spon_Data_V1'))
+all_path_dic.pop(4)
+all_path_dic.pop(6)
 
 import warnings
 warnings.filterwarnings("ignore")
+#%%########################## 1. DEPICT AVR STRENGTH EXAMPLE ######################
+avr_response_raw = c_spon.mean(1)
+decay_response = np.sort(avr_response_raw)[::-1] # decay order.
+response_ensemble = c_spon>1
+response_ensemble = response_ensemble.sum(1) # this method have some problems..
+decay_ensemble = np.sort(response_ensemble)[::-1]
+# get all model classified id.
+analyzer = UMAP_Analyzer(ac = ac,umap_model = c_model,spon_frame = c_spon)
+analyzer.Train_SVM_Classifier()
+spon_train = analyzer.spon_label
+spon_ons = np.where(spon_train>0)[0]
+spon_offs = np.where(spon_train==0)[0]
+avr_spon_on = c_spon.iloc[spon_ons,:].mean(1)
+decay_on = np.sort(avr_spon_on)[::-1]
+avr_spon_off = c_spon.iloc[spon_offs,:].mean(1)
+decay_off = np.sort(avr_spon_off)[::-1]
+#%% Plot CDF of all spon data and avr 
+plt.clf()
+plt.cla()
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,4),dpi = 180,width_ratios=[2, 1],sharey=False)
+ax[0].axvline(x = np.median(avr_spon_on),color = 'gray',linestyle = '--')
+ax[0].axvline(x = np.median(avr_spon_off),color = 'gray',linestyle = '--')
+ax[0].text(-0.5,0.3,f'Response Diff={(np.median(avr_spon_on)-np.median(avr_spon_off)):.2f}')
+# ymax=decay_response[2676]
+sns.lineplot(y = np.array(range(len(decay_off)))/len(decay_off),x = decay_off,ax = ax[0])
+# add an end point for on frames.
+sns.lineplot(y = np.append(np.array(range(len(decay_on)))/len(decay_on),1),x = np.append(decay_on,decay_response.min()),ax = ax[0])
+# sns.lineplot(y = np.array(range(len(decay_off)))/len(decay_off),x = decay_off,ax = ax)
+# sns.lineplot(y = np.array(range(len(decay_ensemble)))/len(decay_ensemble),x = decay_ensemble,ax = ax)
+# ax.set_xlim(524,-20)
+ax[0].set_xlim(3,-3)
+ax[0].set_title('CDF of Spontaneous Response Frames')
+ax[0].set_ylabel('Prob.')
+ax[0].set_xlabel('Average Z Score')
+
+# and plot histo graph of spon data.
+seperated_response = pd.DataFrame(0,index = range(len(decay_off)),columns = ['Strength','Type'])
+seperated_response.loc[:,'Type'] = 'Non-Stim Repeats'
+seperated_response.loc[:,'Strength'] = decay_off
+seperated_response2 = pd.DataFrame(0,index = range(len(decay_on)),columns = ['Strength','Type'])
+seperated_response2.loc[:,'Type'] = 'Spon Repeats'
+seperated_response2.loc[:,'Strength'] = decay_on
+plotable_data = pd.concat([seperated_response,seperated_response2])
+# sns.histplot(x = decay_response,stat="density",alpha = 0.5,ax = ax[1])
+# sns.histplot(x = decay_on,stat="density",alpha = 0.5,ax = ax[1])
+g = sns.histplot(data=plotable_data,x = 'Strength', stat='probability',hue = 'Type', common_norm=False,ax = ax[1])
+
+sns.move_legend(g, "lower center", bbox_to_anchor=(.75, 1), ncol=1, title=None, frameon=False)
+ax[1].yaxis.set_label_position('right')
+ax[1].yaxis.tick_right() 
+ax[1].set_ylabel('Probability Density')
+ax[1].set_xlabel('Average Z Score')
+fig.tight_layout()
+plt.show()
+
+
+#%%########################## 2. STRENGTH DECAY ALL POINTS ############################################
+all_spon_strength = pd.DataFrame(columns = ['Strength','Type','Loc'])
+for i,cloc in tqdm(enumerate(all_path_dic)):
+    cloc_name = cloc.split('\\')[-1]
+    ac = ot.Load_Variable_v2(cloc,'Cell_Class.pkl')
+    c_spon = ot.Load_Variable(cloc,'Spon_Before.pkl')
+    c_model = ot.Load_Variable(cloc,'All_Stim_UMAP_3D_20comp.pkl')
+    c_analyzer = UMAP_Analyzer(ac = ac,umap_model=c_model,spon_frame=c_spon)
+    c_analyzer.Train_SVM_Classifier()
+    c_spon_series = c_analyzer.spon_label
+    c_spon_response = np.array(c_spon.mean(1))
+    for j in range(len(c_spon_series)):
+        if c_spon_series[j] == 0:
+            all_spon_strength.loc[len(all_spon_strength),:] = [c_spon_response[j],'Unclassified Spontaneous',cloc_name]
+        else:
+            all_spon_strength.loc[len(all_spon_strength),:] = [c_spon_response[j],'Classified Spontaneous',cloc_name]
+    # spon_ons = np.where(c_spon_series>0)[0]
+    # spon_offs = np.where(c_spon_series==0)[0]
+    # avr_spon_on = c_spon.iloc[spon_ons,:].mean(1)
+    # avr_spon_off = c_spon.iloc[spon_offs,:].mean(1)
+
+#%% Plot 
+on_series = np.array(all_spon_strength.groupby('Type').get_group('Classified Spontaneous')['Strength'])
+off_series = np.array(all_spon_strength.groupby('Type').get_group('Unclassified Spontaneous')['Strength'])
+decay_on = np.sort(on_series)[::-1]
+decay_off = np.sort(off_series)[::-1]
+
+plt.clf()
+plt.cla()
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7,4),dpi = 180,width_ratios=[2, 1],sharey=False)
+ax[0].axvline(x = np.median(on_series),color = 'gray',linestyle = '--')
+ax[0].axvline(x = np.median(off_series),color = 'gray',linestyle = '--')
+ax[0].text(-0.4,0.3,f'Response Diff={(np.median(on_series)-np.median(off_series)):.2f}')
+# ymax=decay_response[2676]
+sns.lineplot(y = np.array(range(len(decay_off)))/len(decay_off),x = decay_off,ax = ax[0])
+# add an end point for on frames.
+sns.lineplot(y = np.append(np.array(range(len(decay_on)))/len(decay_on),1),x = np.append(decay_on,decay_response.min()),ax = ax[0])
+# sns.lineplot(y = np.array(range(len(decay_off)))/len(decay_off),x = decay_off,ax = ax)
+# sns.lineplot(y = np.array(range(len(decay_ensemble)))/len(decay_ensemble),x = decay_ensemble,ax = ax)
+# ax.set_xlim(524,-20)
+ax[0].set_xlim(3,-3)
+ax[0].set_title('CDF of Spontaneous Response Frames')
+ax[0].set_ylabel('Prob.')
+ax[0].set_xlabel('Average Z Score')
+g = sns.histplot(data=all_spon_strength,x = 'Strength', stat='probability',hue = 'Type', common_norm=False,ax = ax[1],bins = 50)
+sns.move_legend(g, "lower center", bbox_to_anchor=(.75, 1), ncol=1, title=None, frameon=False)
+ax[1].yaxis.set_label_position('right')
+ax[1].yaxis.tick_right()
+ax[1].set_ylabel('Probability Density')
+ax[1].set_xlabel('Average Z Score')
+fig.tight_layout()
+plt.show()

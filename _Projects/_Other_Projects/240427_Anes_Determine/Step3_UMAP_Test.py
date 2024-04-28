6574e473b39de11e55d0b4f2e7dced48e3a1379c
adolescent <39371189+adolescent@users.noreply.github.com>
2024-04-28 13:00:24 +0800

    Update Step3_UMAP_Test.py

--- _Projects/_Other_Projects/240427_Anes_Determine/Step3_UMAP_Test.py
+++ _Projects/_Other_Projects/240427_Anes_Determine/Step3_UMAP_Test.py
@@ -19,17 +19,26 @@ import umap.plot
 from Kill_Cache import kill_all_cache
 
 wp = r'D:\#Shu_Data\TailPinch\ChatFlox_Cre\_Stats_All_Points'
-all_frames = ot.Load_Variable(wp,'All_Raw_Parameters.pkl')
+all_frames = ot.Load_Variable(wp,'Normalized_All_Raw_Parameters.pkl')
 all_case = list(set(all_frames['Case']))
 
+
 #%%
-kill_all_cache(r'C:\ProgramData\anaconda3\envs\umapzr')
-reducer = umap.UMAP(n_components=2,n_neighbors=100)
-reducer.fit(np.array(all_frames.iloc[:,:9]))
+# kill_all_cache(r'C:\ProgramData\anaconda3\envs\umapzr')
+reducer = umap.UMAP(n_components=2,n_neighbors=1000)
+data_frames = all_frames.groupby('Case').get_group(all_case[0])
+data_frames = all_frames.drop('Case', axis=1)
+data_frames = data_frames.drop('Time',axis = 1)
+reducer.fit(np.array(data_frames))
 ot.Save_Variable(wp,'Raw_UMAP_N100',reducer)
 #%% Plot series time train
-# cloc_data = all_frames.groupby('Case').getgroup(all_case[0])
+# cloc_data = all_frames.groupby('Case').get_group(all_case[0])
+# scatters = cloc_data.drop('Case', axis=1)
+# scatters = scatters.drop('Time',axis = 1)
+# scatters = reducer.transform(scatters)
+
 scatters = reducer.embedding_
+
 plt.clf()
 plt.cla()
 fig,ax = plt.subplots(nrows=1, ncols=1,figsize = (10,7),dpi = 144,sharex= True)
@@ -39,3 +48,10 @@ cbar.set_label('Time (s)')
 ax.set_xlabel('UMAP 1')
 ax.set_ylabel('UMAP 2')
 ax.set_title(f'UMAP Embedding of All States')
+
+
+#%% Data Viewer
+for i in range(5):
+    cloc_data = all_frames.groupby('Case').get_group(all_case[i])
+    plt.plot(cloc_data['EEG_Below10Hz'])
+

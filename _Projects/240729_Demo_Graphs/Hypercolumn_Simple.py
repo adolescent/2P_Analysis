18e988fa4c1535f726c2812f594a793482861c4b
adolescent <39371189+adolescent@users.noreply.github.com>
2024-08-02 13:02:38 +0800

    demos

--- _Projects/240729_Demo_Graphs/Hypercolumn_Simple.py
+++ _Projects/240729_Demo_Graphs/Hypercolumn_Simple.py
@@ -67,17 +67,31 @@ for i in range(4):
     graph = cv2.line(graph, start,(int(start[0]+base_width*np.cos(angle)),int(start[1]-base_width*np.sin(angle))),color,surface_width)
     
 
-# plot example cells on given location.
+## plot example cells on given location.
+# oval_dist_angled = int(base_width/4)
+# oval_dist_h = base_len/7
+# oval_dist_h = int((oval_dist_h+oval_dist_angled*np.cos(angle))/2)
+# oval_dist_v = int((oval_dist_angled*np.sin(angle))/2)
+# init_oval = (int(A[0]+oval_dist_h*0.7),int(A[1]-oval_dist_v*0.6))
+# for i in range(8):
+#     for j in range(8):
+#         c_oval_cen = (init_oval[0]+i*oval_dist_h+j*oval_dist_v,init_oval[1]-j*oval_dist_v)
+#         graph = cv2.ellipse(graph,c_oval_cen,(20,10),0, 0,360, blob_color,-1)
+## another version, we can plot triangle.
+tri_bais = 4
+tri_h = 8
+tri_lr = 8
 oval_dist_angled = int(base_width/4)
-oval_dist_h = base_len/7
-oval_dist_h = int((oval_dist_h+oval_dist_angled*np.cos(angle))/2)
+oval_dist_h = base_len/8
 oval_dist_v = int((oval_dist_angled*np.sin(angle))/2)
 init_oval = (int(A[0]+oval_dist_h*0.7),int(A[1]-oval_dist_v*0.6))
 for i in range(8):
     for j in range(8):
-        c_oval_cen = (init_oval[0]+i*oval_dist_h+j*oval_dist_v,init_oval[1]-j*oval_dist_v)
-        graph = cv2.ellipse(graph,c_oval_cen,(20,10),0, 0,360, blob_color,-1)
-
+        center = (init_oval[0]+i*oval_dist_h+j*oval_dist_v,init_oval[1]-j*oval_dist_v)
+        vertex1 = (int(center[0]-tri_lr-tri_bais/2), int(center[1]+tri_h))
+        vertex2 = (int(center[0]+tri_lr+tri_bais/2), int(center[1]+tri_h))
+        vertex3 = (int(center[0]+tri_bais/2), int(center[1]-tri_h*2+tri_bais/2))
+        cv2.fillPoly(graph, [np.array([vertex1, vertex2, vertex3])], blob_color)
 
 
 # add orien bars on graph.
@@ -97,4 +111,4 @@ for i in range(4):
 cv2.imshow('Hypercolumn',graph)
 cv2.waitKey(5000)
 cv2.destroyAllWindows()
-cv2.imwrite(r'D:\_GoogleDrive_Files\#Figs\Model_Demo\Hypercolumn_Simple.png',graph)
+cv2.imwrite(r'D:\_GoogleDrive_Files\#Figs\Model_Demo\Hypercolumn_Simple_Triangle.png',graph)
--- _Projects/240729_Demo_Graphs/demo_graph.py
+++ _Projects/240729_Demo_Graphs/demo_graph.py
@@ -27,17 +27,30 @@ for i in range(4):
                         color=c_color, linewidth=2)
 
 # Draw the 49 filled circles in gray color on top of the lines
-circle_radius = 0.36  # Adjust the radius as needed
+# circle_radius = 0.36  # Adjust the radius as needed
+# for i in range(4):
+#     for j in range(4):
+#         x = i * 10 / 6 + 1
+#         y = j * 10 / 6 + 1
+#         circle = plt.Circle((x, y), circle_radius, fill=True, color='gray',zorder=10)
+#         ax.add_artist(circle)
+
+# plot circles.
+tri_size_u = 0.5
+tri_size_lr = 0.25
 for i in range(4):
     for j in range(4):
-        x = i * 10 / 6 + 1
-        y = j * 10 / 6 + 1
-        circle = plt.Circle((x, y), circle_radius, fill=True, color='gray',zorder=10)
-        ax.add_artist(circle)
+        center = [i*10/6+1,j*10/6+1]
+        vertex1 = (center[0]-tri_size_lr, center[1]-tri_size_u/3)
+        vertex2 = (center[0]+tri_size_lr, center[1]-tri_size_u/3)
+        vertex3 = (center[0], center[1]+tri_size_u*2/3)
+        ax.fill([vertex1[0], vertex2[0], vertex3[0]], 
+        [vertex1[1], vertex2[1], vertex3[1]],
+        color='gray', linewidth=0,zorder = 10)
 
 # Hide the x and y axes
 ax.set_axis_off()
 
 # Display the plot
 # plt.show()
-plt.savefig('D:\_GoogleDrive_Files\#Figs\Model_Demo\FC_Smaller.png', transparent=True)
+# plt.savefig('D:\_GoogleDrive_Files\#Figs\Model_Demo\Point_Only.png', transparent=True)
